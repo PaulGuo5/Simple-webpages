9f2ee8cc775280198119b344ea690fa76f4b482d
Glitch (hello-webpage) <none>
2019-02-06 22:17:43 +0000

    💨🍰 Checkpoint ./script.js:2768340/1036 ./style.css:2768340/74

--- script.js
+++ script.js
@@ -3,3 +3,16 @@
 
 // prints "hi" in the browser's dev tools console
 console.log('hi');
+
+function button_detail1_function(){
+  console.log("Button three clicked!");
+// Your code here
+  //insert a new element named "Super big Big Mac"
+  var get_ul = document.getElementsByTagName("ul")[0];
+  var new_li = document.createElement("li");
+  var new_text = document.createTextNode("Super big Big Mac");
+  new_li.appendChild(new_text);
+  get_ul.appendChild(new_li);
+}
+const button_detail1 = document.getElementById("detail1");
+button_detail1.addEventListener('click', button_detail1_function);
