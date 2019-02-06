a380790f9c230cb56b08e8c3fd2aba4b64b78f43
Glitch (hello-webpage) <none>
2019-02-06 23:01:44 +0000

    💡🛰 Checkpoint ./script.js:2768340/313 ./music.html:2768340/4970 ./.glitch-assets:2768340/420 ./index.html:2768340/19

--- script.js
+++ script.js
@@ -20,4 +20,13 @@ function button_detail2_function(){
   get_ul.className = "display";
 }
 const button_detail2 = document.getElementById("detail2");
-button_detail1.addEventListener('click', button_detail1_function);
+button_detail2.addEventListener('click', button_detail2_function);
+
+
+function button_detail3_function(){
+  console.log("Button detail3 clicked!");
+  var get_ul = document.getElementsByTagName("ul")[3];
+  get_ul.className = "display";
+}
+const button_detail3 = document.getElementById("detail3");
+button_detail3.addEventListener('click', button_detail3_function);
