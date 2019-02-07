85b876fe8fc2d7bfeef9da3f34c47f1966a52d91
Glitch (hello-webpage) <none>
2019-02-07 00:51:43 +0000

    📮🐈 Checkpoint ./script.js:2768340/379 ./script2.js:2768340/1121 ./movies.html:2768340/1

---
+++ script2.js
@@ -0,0 +1,16 @@
+function button_moreMovie_function(){
+  console.log("Button moreMovie clicked!");
+  var get_table = document.getElementById("movieTable");
+  var new_tr = document.createElement("tr");
+  var new_td1 = document.createElement("td");
+  var new_td2 = document.createElement("td");
+  var new_td3 = document.createElement("td");
+  var new_td4 = document.createElement("td");
+  var new_text1 = document.createTextNode("Harry Potter");
+  var new_text2 = document.createTextNode("2001-2016");
+  new_li.appendChild(new_text);
+  get_ul.appendChild(new_li);
+
+}
+const button_moreMovie = document.getElementById("moreMovie");
+button_moreMovie.addEventListener('click', button_moreMovie_function);
