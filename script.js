93176fecebd53f8cdc158c02a91749868d9e7898
Glitch (hello-webpage) <none>
2019-02-10 00:51:10 +0000

    🍘🚪 Checkpoint ./music.html:2768340/98 ./style.css:2768340/58 ./script.js:2768340/103

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log('hi');
 
 function button_detail1_function(){
   console.log("Button detail1 clicked!");
-  var get_ul = document.getElementsByTagName("ul")[1];
+  var get_ul = document.getElementById("ul_infor_1");
   get_ul.className = "display";
 }
 const button_detail1 = document.getElementById("detail1");
@@ -16,7 +16,7 @@ button_detail1.addEventListener('click', button_detail1_function);
 
 function button_detail2_function(){
   console.log("Button detail2 clicked!");
-  var get_ul = document.getElementsByTagName("ul")[2];
+  var get_ul = document.getElementById("ul_infor_2");
   get_ul.className = "display";
 }
 const button_detail2 = document.getElementById("detail2");
@@ -25,7 +25,7 @@ button_detail2.addEventListener('click', button_detail2_function);
 
 function button_detail3_function(){
   console.log("Button detail3 clicked!");
-  var get_ul = document.getElementsByTagName("ul")[3];
+  var get_ul = document.getElementById("ul_infor_3");
   get_ul.className = "display";
 }
 const button_detail3 = document.getElementById("detail3");
