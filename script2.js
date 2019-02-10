166f96363fbd0b136cc0b6e6f0ddf20f47ea11d5
Glitch (hello-webpage) <none>
2019-02-10 05:29:16 +0000

    🍟📌 Checkpoint ./movies.html:2768340/1729 ./script2.js:2768340/411 ./style.css:2768340/12

--- script2.js
+++ script2.js
@@ -9,7 +9,7 @@ function button_moreMovie_function(){
   var new_text1 = document.createTextNode("Harry Potter");
   var new_text2 = document.createTextNode("2001-2016");
   var new_text3 = document.createTextNode("Daniel Radcliff");
-  var new_text4 = document.createTextNode("Harry Potter is a British-American film series based on the Harry Potter novels by author J. K. Rowling. The series is distributed by Warner Bros. and consists of eight fantasy films, beginning with Harry Potter and the Philosopher's Stone (2001) and culminating with Harry Potter and the Deathly Hallows – Part 2 (2011).[2][3] A spin-off prequel series that will consist of five films started with Fantastic Beasts and Where to Find Them (2016), marking the beginning of the Wizarding World shared media franchise.");
+  var new_text4 = document.createTextNode("Harry Potter is a British-American film series based on the Harry Potter novels by author J. K. Rowling.");
   new_td1.appendChild(new_text1);
   new_td2.appendChild(new_text2);
   new_td3.appendChild(new_text3);
